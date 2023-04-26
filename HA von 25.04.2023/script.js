d184bb6562dc9e9e43e8cb59da051a4011889a7e
OlgaDi <129493572+olgamgsd2603@users.noreply.github.com>
2023-04-26 21:43:50 +0200

    Add files via upload

---
+++ HA von 25.04.2023/script.js
@@ -0,0 +1,29 @@
+
+//    for(let i = 0; i <= 10; i += 2) {
+//        console.log(i)
+//    }
+
+//  for(let i = 55; i >= 20; i--) {
+//      console.log(i)
+//  }
+
+//    const numbers = [3, 5, 11, 2, 8, 1, 6]
+//    const numbers_squared = []
+//     for(let i = 0; i < numbers.length; i++){ 
+//       console.log(numbers[i])
+//        }
+//     for(let i = 0; i < numbers.length; i++){
+//         let squared = numbers[i] ** 2
+//         numbers_squared.push(squared)
+//     }
+//        console.log(numbers_squared)
+//        let last_elem = numbers_squared[6]
+//        console.log(last_elem)
+       
+const user = {
+    first_name: "Olga", 
+    last_name: "Dirksen", 
+    age: 20, 
+    salary: 500
+}
+ console.log(` User’s name is: ${user.first_name} ${user.last_name}. He is ${user.age}`)
